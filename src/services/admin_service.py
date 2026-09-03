cb2672e54514d987f0d129787da8d000072514fa
Isaac Urban <kpomassiurban@gmail.com>
2026-09-03 16:21:29 +0100

    created the abstract method to create update and remove menu for admin

--- src/services/admin_service.py
+++ src/services/admin_service.py
@@ -1,10 +1,23 @@
 from abc import ABC, abstractmethod
 
+from schemas.menu import MenuCreate, MenuResponse, MenuUpdate
 from src.db_models.user import User
 from src.schemas.menu_item import MenuItemResponse, MenuItemCreate, MenuItemUpdate
 
 
 class AdminService(ABC):
+
+    @abstractmethod
+    def create_menu(self, user: User, menu: MenuCreate) -> MenuResponse:
+        pass
+
+    @abstractmethod
+    def update_menu(self, user: User, menu_id: int, menu: MenuUpdate) -> MenuResponse:
+        pass
+
+    @abstractmethod
+    def remove_menu(self, user: User, menu_id: int) -> None:
+        pass
 
     @abstractmethod
     def add_menu_item(self, user: User, item: MenuItemCreate) -> MenuItemResponse:
