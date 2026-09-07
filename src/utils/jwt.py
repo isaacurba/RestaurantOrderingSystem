fa26794be087a9767a619b1fbc0fe5a5dea5e87e
Isaac Urban <kpomassiurban@gmail.com>
2026-09-07 14:13:26 +0100

    removed the minuete from env

--- src/utils/jwt.py
+++ src/utils/jwt.py
@@ -5,7 +5,7 @@ from jose import jwt
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 def create_access_token(user_id: int):
     expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
