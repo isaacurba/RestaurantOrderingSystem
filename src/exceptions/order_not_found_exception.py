6891354e34b6a41626e27305030b5a61641694ca
Vincent Opurum <vincentchimene@gmail.com>
2026-09-09 10:13:19 +0100

    customer Service

---
+++ src/exceptions/order_not_found_exception.py
@@ -0,0 +1,9 @@
+from fastapi import status
+
+from src.exceptions.app_exception import AppException
+
+
+class OrderNotFoundException(AppException):
+
+    def __init__(self, message: str):
+        super().__init__(message, status.HTTP_404_NOT_FOUND)
